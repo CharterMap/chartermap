d0fda3ac131fa82bb58ea4f7f48c83c51408c20d
CharterMap <c.austin.gonzalez@gmail.com>
2024-06-06 22:22:28 +0000

    Skeleton implementation of tinymce as text editor

--- XMLInlineEditor/editor.php
+++ XMLInlineEditor/editor.php
@@ -3,19 +3,21 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Multilayer Inline Editor for XML</title>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector:'#mytextarea'
+        });
+    </script>
 </head>
 
 <body>
-    <section>
-        <h1>Multilayer Inline Editor for XML</h1>
-        <textarea   id="textarea_milex"
-                    name="milex"
-                    rows="15"
-                    cols="80"
-                    placeholder="Put charters in me">
-        </textarea>
-    </section>
+    <h1>Multilayer Inline Editor for XML</h1>
+    <form method="post">
+        <textarea id="mytextarea">Put charters in me</textarea>
+    </form>
 </body>
 
 </html>
